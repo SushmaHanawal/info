88b15701be129f9156494f9ad3dd282664b68dac
Sushma <sushmapatil123@gmail.com>
2017-07-24 21:36:39 +0530

    adding sql queries for Product database

--- sql_problems/practice_sql_set02_Products.sql
+++ sql_problems/practice_sql_set02_Products.sql
@@ -40,3 +40,24 @@ where 2011_sales.year= 2011 and 2012_sales.year = 2012 and
 and p.`PRODUCT_ID` = 2011_sales.`PRODUCT_ID`
 and p.`PRODUCT_ID` = 2012_sales.`PRODUCT_ID`;
 
+
+-- Q4: Write a query to select the top product sold in each year
+
+select * from sales;
+
+select p.`PRODUCT_NAME` , A.year, A.quantity from (select year, max(quantity) as quantity from sales group by year) A 
+inner join sales s on (s.year = A.year and s.`QUANTITY` = A.`QUANTITY`)
+inner join products p ON p.`PRODUCT_ID` = s.product_id
+order by year asc;
+
+-- Q5: Write a query to find the average quantity of product sold so far
+
+select p.product_name, avg.average as average_quantity 	from (select product_id, sum(quantity)/count(quantity) as average from sales group by product_id) avg inner join products p on p.`PRODUCT_ID` = avg.`PRODUCT_ID`;
+
+-- Q6: Write a query to find the products whose sold more than 10 quantities in a given year
+select product_name from products where product_id in (select product_id from sales where quantity  > 10);
+
+-- or
+select p.product_name, s.quantity, s.year from products p inner join sales s 
+on p.product_id = s.product_id where s.quantity > 10;
+
